4557d5224a63441fcb12aa7815b5f99a6c4343c8
Manoj-0921 <141904180+Manoj-0921@users.noreply.github.com>
2023-11-06 17:18:18 +0530

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,19 @@
+var randomvariable = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomvariable + ".png";
+var randomDiceImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImageSource);
+var randomvariable2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomvariable2 + ".png";
+var randomDiceImageSource2 = "images/" + randomDiceImage2;
+var image2 = document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", randomDiceImageSource2);
+if (randomvariable > randomvariable2) {
+  document.querySelector("h1").innerHTML = "!palyer 1 wins";
+} else if (randomvariable < randomvariable2) {
+  document.querySelector("h1").innerHTML = "!palyer 2 win ";
+} else {
+  document.querySelector("h1").innerHTML = "draw";
+}
